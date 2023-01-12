e51b79a65d937a46cf4f6c877f3bf1efc5ce2560
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-01-13 01:36:30 +0530

    Refactor: Table Body Code

--- src/components/Table/TableBody.tsx
+++ src/components/Table/TableBody.tsx
@@ -10,20 +10,23 @@ interface TableBodyProps<T> {
 function TableBody<T>({ columns, data, rowKey }: TableBodyProps<T>) {
   return (
     <>
-      {data.map((item) => (
-        <tr key={item[rowKey as keyof typeof item] as Key}>
-          {columns.map((column) => (
-            <td key={column.key}>
-              {/* If `dataIndex` exists that is shown instead of `render` */}
-              {column.dataIndex
-                ? (item[column.dataIndex as keyof typeof item] as ReactNode)
-                : column.render
-                ? column.render()
-                : null}
-            </td>
-          ))}
-        </tr>
-      ))}
+      {data.map((item) => {
+        const id = item[rowKey as keyof typeof item] as Key
+        return (
+          <tr key={id}>
+            {columns.map((column) => {
+              const { dataIndex, render, key } = column
+              const value = item[dataIndex as keyof typeof item] as ReactNode
+              return (
+                <td key={key}>
+                  {/* If `dataIndex` exists that is shown instead of `render` */}
+                  {dataIndex ? value : render ? render() : null}
+                </td>
+              )
+            })}
+          </tr>
+        )
+      })}
     </>
   )
 }
