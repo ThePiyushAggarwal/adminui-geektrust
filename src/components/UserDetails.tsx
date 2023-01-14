45749786f3d77b2ffff4fc9517129bc289743663
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-01-14 13:52:52 +0530

    Feat: Integrate Redux for users states

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,14 +1,25 @@
-import { useMemo, useState } from "react"
+import { useMemo, useState, useEffect } from "react"
 import { GET_USERS_ENDPOINT } from "../api/getUsersEndpoint"
 import useQuery from "../api/useQuery"
+import { useAppDispatch, useAppSelector } from "../store/hooks"
+import { setUsers } from "../store/userSlice"
 import { User } from "../types/Users"
 import { filterOnSearch } from "../utils/filterOnSearch"
 import SearchBar from "./SearchBar"
 import UserTable from "./UserTable"
 
 function UserDetails() {
+  const dispatch = useAppDispatch()
   const { data } = useQuery<User[]>(GET_USERS_ENDPOINT)
   const [searchTerm, setSearchTerm] = useState<string>("")
+
+  const { users } = useAppSelector((state) => state.user)
+
+  useEffect(() => {
+    if (data?.length) {
+      dispatch(setUsers(data))
+    }
+  }, [data])
 
   /**
    * TODO I want to use debouncing here.
@@ -16,8 +27,8 @@ function UserDetails() {
    * Let's see.
    */
   const filteredData = useMemo(
-    () => data && filterOnSearch<User>(data, searchTerm, ["id"]),
-    [searchTerm, data]
+    () => filterOnSearch<User>(users, searchTerm, ["id"]),
+    [searchTerm, users]
   )
 
   return (
