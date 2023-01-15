d16fb8a04382e80217d928cf094b28588bc36c0f
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-01-15 22:47:54 +0530

    Fix: Footer position fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="bg-white/80 absolute bottom-0 right-0 py-2 px-2 rounded-tl-lg">
+    <div className="bg-white/80 fixed bottom-0 right-0 py-2 px-2 rounded-tl-lg">
       Made by @
       <a
         href="https://github.com/ThePiyushAggarwal"
